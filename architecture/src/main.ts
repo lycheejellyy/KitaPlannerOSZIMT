3ab1dba5c5709604b442e646db5b3d1e10fb900c
lycheejellyy <100092150+lycheejellyy@users.noreply.github.com>
2024-12-11 13:31:06 +0100

    fix: decluttered

--- architecture/src/main.ts
+++ architecture/src/main.ts
@@ -1,10 +1,7 @@
-import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import { bootstrapApplication} from '@angular/platform-browser';
 
 import {AppComponent} from './app/app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
-import { HomeComponent } from './app/home/home.component';
-import { routes } from './app/app.routes';
+
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.log(err));
