82db9a336d44c18db2b1fb7bbe82a0d33bee6f85
lycheejellyy <100092150+lycheejellyy@users.noreply.github.com>
2025-01-08 03:02:26 +0100

    feat: child card date formatted

--- architecture/src/app/home/home.component.ts
+++ architecture/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { NavBarComponent } from "../components/nav-bar/nav-bar.component";
 import { ChildCardComponent } from "../components/child-card/child-card.component";
 import { ChildService } from '../services/child.services';
 import { CommonModule } from '@angular/common';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -16,8 +17,17 @@ export class HomeComponent implements OnInit {
   cards: any[] = [];
 
   constructor(private childService: ChildService) {}
+
   ngOnInit(): void {
-    this.childService.getChildren().subscribe(data => this.cards = data);
+    this.childService.getChildren().subscribe(data => {
+      this.cards = data.map(card => ({
+        ...card,
+        birthdate: this.formatDate(card.birthdate)
+      }));
+    });
   }
 
+  private formatDate(dateString: string): string {
+    return format(new Date(dateString), 'dd.M.yyyy'); 
+  }
 }
